f9af94ddcac10650e8c4de88262db077fd76df8c
yanuarkrist <49681988+yanuarkrist@users.noreply.github.com>
2019-04-23 23:21:00 +0700

    Add files via upload

---
+++ Tugas17Aprill.java
@@ -0,0 +1,29 @@
+package tugas17aprill;
+public class Tugas17Aprill {
+    public static void main(String[] args) {
+        Manusia normal = new Manusia();
+        Pekerja kerja = new Pekerja();
+        Mahasiswa kuliah = new Mahasiswa();
+        Sekolah siswa = new Sekolah();
+       
+       normal.inputNama();
+       normal.inputUmur();
+       
+       siswa.inputUmur();
+       siswa.inputNIS();
+       
+       kuliah.inputUmur();
+       kuliah.inputNIM();
+       
+       kerja.inputUmur();
+       kerja.inputNIK();
+       
+      
+       
+       
+      
+       
+       
+    }
+    
+}
